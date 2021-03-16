42ee26b29c7240da0808fc9bc03aadf7d017c20e
Semyon Dokiichuk <orgnitro@gmail.com>
2021-03-16 23:48:03 +0200

    redux added

--- react-ui/src/redux/store.js
+++ react-ui/src/redux/store.js
@@ -1,13 +1,10 @@
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import combinedReducer from './reducers'
 
 const store = createStore(
   combinedReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  applyMiddleware(thunk)
 )
 
 export default store
